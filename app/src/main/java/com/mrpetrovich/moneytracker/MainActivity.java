d223caec512fe04cbb510f0e4825ce676a153201
Mr.Petrovich <ivanchenkostas@gmail.com>
2018-03-18 23:47:35 +0300

    немного изменений

--- app/src/main/java/com/mrpetrovich/moneytracker/MainActivity.java
+++ app/src/main/java/com/mrpetrovich/moneytracker/MainActivity.java
@@ -1,7 +1,6 @@
 package com.mrpetrovich.moneytracker;
 
 import android.app.Activity;
-import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -10,7 +9,6 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
